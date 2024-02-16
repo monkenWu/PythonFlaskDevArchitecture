9e97b20fa23b3a7a6f65af1c70cbf876a5fe1642
MonkenWu <monkenWu@monken.tw>
2024-02-17 01:23:54 +0800

    2024.02.17 added table relationship

--- model/orm/Task.py
+++ model/orm/Task.py
@@ -1,12 +1,15 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 from . import Base
 
 class Task(Base):
     __tablename__ = 'tasks'
     id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)  # 外键
     name = Column(String, nullable=False)
     status = Column(Integer, default=0)
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
     deleted_at = Column(DateTime, nullable=True)
+    user = relationship("User", back_populates="tasks")
